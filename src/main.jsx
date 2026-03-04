3e2465e823ceeab852013a9eee6055f3f8f4edd0
Zach Lajeunesse <zaclajeunesse1@gmail.com>
2026-03-04 15:13:21 -0500

    Remove ThemeContext from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
-import { ThemeProvider } from './context/ThemeContext'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ThemeProvider>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
-      </ThemeProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
+```
+
+Two lines removed — the ThemeProvider import and the `<ThemeProvider>` wrapper. Save, then:
+```
